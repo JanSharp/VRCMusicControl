03e452d76186416c0db5b1c70594dc560c5c2f48
JanSharp <steinmeister36@gmail.com>
2023-08-03 22:04:54 +0200

    Cleanup MusicArea, nothing changed

--- Runtime/MusicArea.cs
+++ Runtime/MusicArea.cs
@@ -12,31 +12,26 @@ namespace JanSharp
         public bool useDefaultPriority = true;
         public int priority = 0;
         private uint id;
+        // To support having multiple trigger colliders on one object, as the enter and exit events get fired
+        // for each collider on the object, so this keeps track of how many colliders the player is in.
         private int triggerCount;
 
         public override void OnPlayerTriggerEnter(VRCPlayerApi player)
         {
-            if (player.isLocal)
-            {
-                triggerCount++;
-                if (triggerCount == 1)
-                    if (useDefaultPriority)
-                        id = musicForThisArea.AddThisMusic();
-                    else
-                        id = musicForThisArea.AddThisMusic(priority);
-            }
+            if (!player.isLocal || (++triggerCount) > 1)
+                return;
+            id = useDefaultPriority
+                ? musicForThisArea.AddThisMusic()
+                : musicForThisArea.AddThisMusic(priority);
         }
 
         public override void OnPlayerTriggerExit(VRCPlayerApi player)
         {
-            if (player.isLocal)
-            {
-                if (triggerCount == 0)
-                    return;
-                triggerCount--;
-                if (triggerCount == 0)
-                    musicForThisArea.Manager.RemoveMusic(id);
-            }
+            // triggerCount == 0 is just there to make sure this script doesn't break when it is placed at the
+            // spawn point of the world. It still has undefined behaviour when doing that though.
+            if (!player.isLocal || triggerCount == 0 || (--triggerCount) > 0)
+                return;
+            musicForThisArea.Manager.RemoveMusic(id);
         }
     }
 }
