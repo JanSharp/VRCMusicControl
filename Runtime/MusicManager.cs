2453f78199d2ab83759003312e9e5bd4ae69aeae
JanSharp <steinmeister36@gmail.com>
2023-08-04 21:30:40 +0200

    Add DefaultExecutionOrder attr to MusicManager
    Specifically to help with using the music manager system with custom
      scripts which interact with it in their Start function

--- Runtime/MusicManager.cs
+++ Runtime/MusicManager.cs
@@ -6,6 +6,7 @@ using VRC.Udon.Common;
 
 namespace JanSharp
 {
+    [DefaultExecutionOrder(-2000)]
     [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
     public class MusicManager : UdonSharpBehaviour
     {
